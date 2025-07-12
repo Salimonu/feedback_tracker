30fda4b9f94813edbd5d27cfe1061a9433d12465
Hammed <salimonuh@gmail.com>
2025-07-12 18:34:17 +0100

    feedback_tracker_12th_07

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [score, setScore] = useState(0);
+
+  function handleCount() {
+    setCount(count => count + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="box">
+      <h1>Feedback Tracker ❤️ </h1>
+      <Display count={count} score={score} />
+      <div className="button__container">
+        <Button color="green" score={setScore} value={1} onCount={handleCount}>
+          Good
+        </Button>
+        <Button color="blue" score={setScore} value={0} onCount={handleCount}>
+          Neutral
+        </Button>
+        <Button color="red" score={setScore} value={-1} onCount={handleCount}>
+          Bad
+        </Button>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+function Button({ color, value, score, onCount, children }) {
+  function handleScore() {
+    score(score => score + value);
+
+    onCount();
+  }
+
+  return (
+    <div className={`button ${color}`} onClick={handleScore}>
+      {children}
+    </div>
+  );
+}
+
+function Display({ count, score }) {
+  let aveScore;
+  if (count) {
+    aveScore = score / count;
+  } else aveScore = 0;
+  return (
+    <div className="display">
+      <p>
+        Total feedback count: <span className="count">{count}</span>
+      </p>
+      <p>
+        Average score:
+        <span className={aveScore > 0 ? 'count' : 'negative'}>{aveScore}</span>
+      </p>
+      <p>Percentage of positive feedback:</p>
+    </div>
+  );
+}
